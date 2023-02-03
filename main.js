a93a960b647cf2e1fe468ff98d484739755c8511
Michael Tobechukwu Nwokike <76704861+Michael-Tobechukwu@users.noreply.github.com>
2023-02-03 11:52:03 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,122 @@
+//Like/dislike button
+function myLike(x) {
+  x.classList.toggle("fa-thumbs-down");
+}
+
+//Show more button
+function readMore() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more-text");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Show more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Show less";
+    moreText.style.display = "inline";
+  }
+}
+
+//More icons button
+function moreIcons() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more-icons");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "&#9776;";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "";
+    moreText.style.display = "inline";
+  }
+}
+
+//More icons II button
+function moreIconsII() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more-iconsII");
+  var btnText = document.getElementById("myBtnII");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "&#9776;";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "";
+    moreText.style.display = "inline";
+  }
+}
+
+//More icons III button
+function moreIconsIII() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more-iconsIII");
+  var btnText = document.getElementById("myBtnIII");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "&#9776;";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "";
+    moreText.style.display = "inline";
+  }
+}
+//More icons II button
+function moreIconsIV() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more-iconsIV");
+  var btnText = document.getElementById("myBtnIV");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "&#9776;";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "";
+    moreText.style.display = "inline";
+  }
+}
+
+//More icons II button
+function moreIconsV() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more-iconsV");
+  var btnText = document.getElementById("myBtnV");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "&#9776;";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "";
+    moreText.style.display = "inline";
+  }
+}
+
+//More icons VI button
+function moreIconsVI() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more-iconsVI");
+  var btnText = document.getElementById("myBtnVI");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "&#9776;";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "";
+    moreText.style.display = "inline";
+  }
+}

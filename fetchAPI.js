87c00b4eeae7a3b02d24cece8541beb3f4b2aad9
Michael Tobechukwu Nwokike <76704861+Michael-Tobechukwu@users.noreply.github.com>
2023-05-17 17:59:36 +0100

    Update fetchAPI.js

--- fetchAPI.js
+++ fetchAPI.js
@@ -75,98 +75,174 @@ document.getElementById("likeBtn7").addEventListener("click", likePost);
 document.getElementById("likeBtn8").addEventListener("click", likePost);
 
 //Put request to like a post
-function likePost() {
-  fetch(`${api}/pins/:id/like`, {
+const userId = 456;
+
+async function likePost() {
+  const response = await fetch("${api}/pins/:id/like", {
     method: "PUT",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      userId: 456,
+      userId,
     }),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log("Post liked successfully");
-    })
-    .catch((error) => console.error(error));
+  });
+
+  const data = await response.json();
+
+  if (data.success) {
+    console.log("Post liked successfully");
+  } else {
+    console.log("Error liking post:", data.error);
+  }
 }
-likePost();
 
-// //Post request to comment on post
-// function commentOnPost() {
-//   fetch(`${api}/pins/comment/:id`, {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//       Authorization: "Bearer <token>",
-//     },
-//     body: JSON.stringify({
-//       userId: 123,
-//       postId: 456,
-//       comment: "This is a great post!",
-//     }),
-//   })
-//     .then((response) => response.json())
-//     .then((data) => console.log(data))
-//     .catch((error) => console.error(error));
-// }
-// commentOnPost();
+//Post request to comment on post
+const token = "<token>";
 
-//Get request to download post
-function downloadPost() {
-  fetch(`${api}/pins/download/:id`)
-    .then((response) => response.blob())
-    .then((blob) => {
-      const url = URL.createObjectURL(blob);
-      const a = document.createElement("a");
-      a.href = url;
-      a.download = "post.pdf";
-      a.click();
-    })
-    .catch((error) => console.error(error));
-}
-downloadPost();
-
-//Post request to save post
-function savePost() {
-  fetch(`${api}/pins/save/:id`, {
+async function commentOnPost(userId, postId, comment) {
+  const response = await fetch("${api}/pins/comment/:id", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
     },
     body: JSON.stringify({
-      title: "Post Title",
-      content: "This is the content of the post",
+      userId,
+      postId,
+      comment,
     }),
-  })
-    .then((response) => response.json())
-    .then((data) => console.log(data))
-    .catch((error) => console.error(error));
+  });
+
+  const data = await response.json();
+
+  console.log(data);
 }
-savePost();
+
+//Get request to download post
+async function downloadPost() {
+  try {
+    const currentPostImg = document.querySelector(".post-img");
+    const currentPostId = currentPostImg.dataset.id;
+    const response = await fetch(`${api}/pins/download/${currentPostId}`);
+    
+    if (!response.ok) {
+      throw new Error('Failed to download the post.');
+    }
+    
+    const blob = await response.blob();
+
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement("a");
+
+    // Get the MIME type of the image
+    const mimeType = currentPostImg.src.split(/[?#]/)[0].split(/\//).pop();
+
+    // Set the download attribute of the anchor element
+    a.download = `post-${currentPostId}.${mimeType}`;
+
+    // Set the href attribute of the anchor element to the generated URL
+    a.href = url;
+
+    // Click the anchor element to download the file
+    a.click();
+  } catch (error) {
+    console.error('Error while downloading the post:', error);
+    // Handle the error appropriately (e.g., display an error message to the user)
+  }
+}
+
+//Post request to save post
+function savePost() {
+  // Get the element with the class of 'post-img' using the querySelector() method.
+  const currentPostImg = document.querySelector(".post-img");
+
+  // Get the ID of the post from the element's dataset.
+  const currentPostId = currentPostImg.dataset.id;
+
+  // Make a request to the API to get the post data.
+  fetch(`${api}/pins/${currentPostId}`)
+    .then((response) => response.json())
+    .then((data) => {
+      // Get the title and content of the post from the API response.
+      const title = data.title;
+      const content = data.content;
+
+      // Create a JSON object with the title and content of the post.
+      const postData = {
+        title,
+        content,
+      };
+
+      // Make a request to the API to save the post.
+      fetch("${api}/pins/save/:id", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(postData),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          // Check the response status code.
+          if (data.status === 200) {
+            // The post was saved successfully.
+            console.log("Post saved successfully.");
+          } else {
+            // There was an error saving the post.
+            console.log("Error saving post: " + data.message);
+          }
+        })
+        .catch((error) => {
+          // There was an error making the request to the API.
+          console.error(error);
+        });
+    })
+    .catch((error) => {
+      // There was an error making the request to the API.
+      console.error(error);
+    });
+}
 
 //Delete post
 function deletePost() {
-  fetch(`${api}/pins/delete/:postId/:deleteId`, {
-    method: "DELETE",
-  })
-    .then((response) => {
-      if (response.ok) {
-        console.log("Post deleted successfully.");
-      } else {
-        console.error("Error deleting post.");
-      }
+  // Get the element with the class of 'post-img' using the querySelector() method.
+  const currentPostImg = document.querySelector(".post-img");
+
+  // Get the ID of the post from the element's dataset.
+  const currentPostId = currentPostImg.dataset.id;
+
+  // Make a request to the API to get the post data.
+  fetch(`${api}/pins/${currentPostId}`)
+    .then((response) => response.json())
+    .then((data) => {
+      // Get the ID of the post from the API response.
+      const deleteId = data.id;
+
+      // Make a request to the API to delete the post.
+      fetch(`${api}/pins/delete/${currentPostId}/${deleteId}`, {
+        method: "DELETE",
+      })
+        .then((response) => {
+          if (response.ok) {
+            console.log("Post deleted successfully.");
+          } else {
+            console.error("Error deleting post.");
+          }
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
     })
     .catch((error) => {
-      console.error("Error:", error);
+      console.error("Error making request to API:", error);
     });
 }
-deletePost();
 
 //Fetch timeline post
 function timeline() {
-  fetch(`${api}/pins/timeline`)
+  const timelineBtn = document.getElementById('timeCapsule');
+  fetch(`${api}pins/timeline`)
     .then((response) => response.json())
     .then((posts) => {
       posts.forEach((post) => {
@@ -175,8 +251,8 @@ function timeline() {
       });
     })
     .catch((error) => console.error(error));
+    timelineBtn.addEventListener('click', timeline)
 }
-timeline();
 
 //Get suggested Accounts of users to follow
 function suggestedAccounts() {
@@ -191,7 +267,6 @@ function suggestedAccounts() {
     })
     .catch((error) => console.error(error));
 }
-suggestedAccounts();
 
 //Subscribe
 function subscribe() {
